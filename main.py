84f2d3a6240ac09fea738c61cdc354f2f3ef70f7
Oblepixa123 <belaevao910@gmail.com>
2025-06-19 09:25:18 +0300

    fixed mistakes

--- main.py
+++ main.py
@@ -10,78 +10,78 @@ from gtts import gTTS
 
 
 def find_answer():
-  print("древняя загадка - бот задает загадку")
+    print("древняя загадка - бот задает загадку")
 
-  answer_user = input("Что можно сломать даже не трогая? ")
-  if answer_user == "тишина":
-    print("ответ верный")
-  else:
-    print("отовет не верный")
+    answer_user = input("Что можно сломать даже не трогая? ")
+    if answer_user == "тишина":
+        print("ответ верный")
+    else:
+        print("отовет не верный")
 
-  user_answer = input("висит груша нельзя скушать, что это? ")
-  if user_answer == "мяу":
-    print("ответ верный")
-  else:
-    print("отовет не верный")
+    user_answer = input("висит груша нельзя скушать, что это? ")
+    if user_answer == "мяу":
+        print("ответ верный")
+    else:
+        print("отовет не верный")
 
-  user = input("да? ")
-  if user == "нет":
-    print("ответ верный")
-  else:
-    print("отовет не верный")
+    user = input("да?")
+    if user == "нет":
+        print("ответ верный")
+    else:
+        print("отовет не верный")
 
 
 
 def age_determination():
-  print("бот спрашивает год рождения и выдает ваш возраст")
+    print("бот спрашивает год рождения и выдает ваш возраст")
 
-  year_user = int(input("в каком году вы родились? "))
-  date_user = datetime.date(year_user, 1, 1)
-  today = datetime.datetime.now().date()
-  result = today - date_user
-  year = result/ 365
-  year = year.days
-  print("вам", year, "лет")
+    year_user = int(input("в каком году вы родились? "))
+    date_user = datetime.date(year_user, 1, 1)
+    today = datetime.datetime.now().date()
+    result = today - date_user
+    year = result/ 365
+    year = year.days
+    print("вам", year, "лет")
 
 
 
 def guess_number():
-  print("бот загадывает число от 1-10")
-  number = random.randint(1, 10)
-  choice_user = input("угадай число загаданное ботом: ")
-  if choice_user == number:
-    print("вы отгдалали число")
-  else:
-    print("вы не отгадали число")
+    print("бот загадывает число от 1-10")
+    number = random.randint(1, 10)
+    choice_user = input("угадай число загаданное ботом: ")
+    if choice_user == number:
+        print("вы отгдалали число")
+    else:
+        print("вы не отгадали число")
 
 
 
 
 def generation_password():
-  print("бот создает пароль из 6 символов")
-  password = ''.join([random.choice(printable) for number in range(6)])
-  password = password.replace('\t', random.choice(printable))
-  password = password.replace('\n', random.choice(printable))
-  password = password.replace('\x0b', random.choice(printable))
-  password = password.replace('\x0c', random.choice(printable))
-  print(password,)
+    print("бот создает пароль из 6 символов")
+    password = ''.join([random.choice(printable) for number in range(6)])
+    password = password.replace('\t', random.choice(printable))
+    password = password.replace('\n', random.choice(printable))
+    password = password.replace('\x0b', random.choice(printable))
+    password = password.replace('\x0c', random.choice(printable))
+    print(password)
 
 
 
 def read_story():
-  print("бот читает историю")
-  language = "ru"
-  file_path="pdf_file.pdf"
-  with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
-      number_pages = pdf_file.pages
-      for page in number_pages:
-          text_pages = page.extract_text()
-          text_pages = text_pages.replace('\n',' ' )
-          audio = gTTS(text=text_pages, lang=language)
-          file_name = "af.mp3"
-          audio.save(file_name)
+    print("бот читает историю")
+    language = "ru"
+    file_path="pdf_file.pdf"
+    with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        number_pages = pdf_file.pages
+        for page in number_pages:
+            text_pages = page.extract_text()
+            text_pages = text_pages.replace('\n',' ' )
+            audio = gTTS(text=text_pages, lang=language)
+            file_name = "af.mp3"
+            audio.save(file_name)
           
-      print(text_pages)
+        print(text_pages)
 
 
 
